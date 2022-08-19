9d411f790d61676c011cd1b6116f3b29e7a338bb
유민욱 <minukyu@theswing.co.kr>
2022-08-19 17:20:00 +0900

    style: 로그인화면 XML 작성
    로그인화면 XML 작성

--- app/src/main/java/com/minux/godminux/LoginActivity.kt
+++ app/src/main/java/com/minux/godminux/LoginActivity.kt
@@ -1,4 +1,11 @@
 package com.minux.godminux
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 로그인 XML 작성
+    }
 }
