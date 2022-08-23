7c6ddba4483ffe7605dce328ea29a3cd01be458a
유민욱 <105332192+minukyu@users.noreply.github.com>
2022-08-23 14:27:35 +0900

    PR 테스트 (#1)
    * wip: 로그인화면 kotlin 파일 생성
    
    로그인화면 kotlin 파일 생성
    
    * style: 로그인화면 XML 작성
    
    로그인화면 XML 작성
    
    * feature: 로그인 API 연동
    
    로그인화면에 로그인 API 연동
    
    * chore: .gitignore 수정
    
    * github PR 템플릿 작성

---
+++ app/src/main/java/com/minux/godminux/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.minux.godminux
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 로그인 XML 작성
+        // 로그인 API 연동
+    }
+}
